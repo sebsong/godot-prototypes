d3a8d029f9bb028ea5ef6e2e953bb622f7861ea2
Sebastian Song <sebastianbsong@gmail.com>
2024-10-05 12:35:11 -0700

    update enemy collider

--- prototype/scripts/Enemy.cs
+++ prototype/scripts/Enemy.cs
@@ -18,7 +18,6 @@ public partial class Enemy : RigidBody2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("ENEMY READY");
 		Randomize();
 	}
 
