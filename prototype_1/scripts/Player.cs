6f4ee350ef3eb3a91ec7ad855c2c340037c08915
Sebastian Song <seby.song@gmail.com>
2024-10-25 23:33:20 -0700

    implement jumping and enter/exit path

--- prototype_1/scripts/Player.cs
+++ prototype_1/scripts/Player.cs
@@ -3,18 +3,14 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	[Export]
-	public Ball Ball;
+	[Export] private Ball _ball;
 
-	[ExportCategory("Movement")]
-	[Export]
-	public float Speed;
-	[Export]
-	public float RideSpeed;
-	[Export]
-	public float Gravity;
+	[ExportCategory("Movement")] [Export] private float _speed;
+	[Export] private float _rideSpeed;
+	[Export] private float _jumpSpeed;
+	[Export] private float _gravity;
 
-	private PathFollow2D _CurrentPath;
+	private PathFollow2D _currentPath;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -24,58 +20,88 @@ public partial class Player : CharacterBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		_RidePath(delta);
 		_Walk();
+		if (_IsPathSet())
+		{
+			Position = _currentPath.Position;
+			_currentPath.Progress += (float)(_rideSpeed * delta);
+			_Jump(delta);
+		}
+		else
+		{
+			_RidePath(delta);
+		}
+
 		MoveAndSlide();
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		base._PhysicsProcess(delta);
-		_ApplyGravity();
+		_ApplyGravity(delta);
 	}
 
 	private void _Walk()
 	{
-		float velocityX = Velocity.X;
+		var velocityX = Velocity.X;
 		if (Input.IsActionJustReleased("walk_left") && Velocity.X < 0)
 		{
 			velocityX = 0;
 		}
+
 		if (Input.IsActionJustReleased("walk_right") && Velocity.X > 0)
 		{
 			velocityX = 0;
 		}
+
 		if (Input.IsActionPressed("walk_left"))
 		{
-			velocityX = -Speed;
+			velocityX = -_speed;
 		}
+
 		if (Input.IsActionPressed("walk_right"))
 		{
-			velocityX = Speed;
+			velocityX = _speed;
 		}
+
 		Velocity = new Vector2(velocityX, Velocity.Y);
 	}
+
 	private void _RidePath(double delta)
 	{
 		if (Input.IsActionJustPressed("ride_path"))
 		{
-			PathFollow2D pathFollow = Ball.FindChild("PathFollow2D") as PathFollow2D;
-			if (_CurrentPath == null)
-			{
-				_CurrentPath = pathFollow;
-			}
-		}
-
-		if (_CurrentPath != null)
-		{
-			Position = _CurrentPath.Position;
-			_CurrentPath.Progress += (float)(RideSpeed * delta);
+			PathFollow2D pathFollow = _ball.FindChild("PathFollow2D") as PathFollow2D;
+			_SetPath(pathFollow);
 		}
 	}
 
-	private void _ApplyGravity()
+	private void _Jump(double delta)
 	{
-		Velocity = new Vector2(Velocity.X, Gravity);
+		if (Input.IsActionJustPressed("jump"))
+		{
+			_ClearPath();
+			Velocity = new Vector2(Velocity.X, -_jumpSpeed);
+		}
+	}
+
+	private void _ApplyGravity(double delta)
+	{
+		Velocity = new Vector2(Velocity.X, Velocity.Y + (float)(_gravity * delta));
+	}
+
+	private bool _IsPathSet()
+	{
+		return _currentPath != null;
+	}
+
+	private void _SetPath(PathFollow2D path)
+	{
+		_currentPath = path;
+	}
+
+	private void _ClearPath()
+	{
+		_currentPath = null;
 	}
 }
